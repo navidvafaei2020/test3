b89b1767914a7618b4f4e3ec7eee9f42ce22bda9
navidvafaei2020 <navidvafaei2020@gmail.com>
2026-02-22 14:54:30 +0100

    we change the text

--- src/main/java/com/test/app/Main.java
+++ src/main/java/com/test/app/Main.java
@@ -2,6 +2,6 @@ package com.test.app;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello to AWS CodeBuild!");
+        System.out.printf("Hello to AWS CodeBuild! AAAAAAAA");
     }
 }
